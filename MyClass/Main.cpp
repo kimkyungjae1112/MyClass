4aa6cea004a8458547b799751dec619a1ed0bfe2
rudwo4861@naver.com <rudwo4861@naver.com>
2023-11-27 21:54:33 +0900

    Vector 클래스 iterator 추가

--- MyClass/Main.cpp
+++ MyClass/Main.cpp
@@ -1,28 +1,24 @@
 #include "MyVector.h"
-#include <string>
+
 int main()
 {
-	MyVector<std::string> mv;
+	VECTOR<int> vec;
+	vec.push_back(60);
+	vec.push_back(20);
+	vec.push_back(50);
+	vec.push_back(20);
+	vec.push_back(10);
+	VECTOR<int>::iterator iter = vec.begin();
 
-	mv.push_back("a");
-	mv.push_back("b");
-
-	for (int i = 0; i < mv.size(); ++i)
+	for (iter; iter != vec.end(); ++iter)
 	{
-		std::cout << mv[i] << " ";
+		cout << *iter << " ";
 	}
-	std::cout << std::endl;
-	std::cout << mv.size();
-	std::cout << std::endl;
-	mv.push_back("c");
-	mv.push_back("d");
-	mv.push_back("e");
-	for (int i = 0; i < mv.size(); ++i)
+	Sort(vec.begin(), vec.end());
+	cout << endl;
+
+	for (auto& i : vec)
 	{
-		std::cout << mv[i] << " ";
+		cout << i << " ";
 	}
-
-	std::cout << std::endl;
-	std::cout << mv.capacity() << std::endl;
-
 }
--- MyClass/MyVector.h
+++ MyClass/MyVector.h
@@ -1,86 +1,204 @@
 #pragma once
+//#include <iostream>
+//
+//template<typename T>
+//class MyVector
+//{
+//	T* content;
+//	int ISize;
+//	int memory_allocation;
+//
+//public:
+//	MyVector() : ISize(-1), memory_allocation(1)
+//	{
+//		content = new T[1];
+//	}
+//
+//	MyVector(int memory_size) : ISize(-1), memory_allocation(memory_size)
+//	{
+//		content = new T[memory_size];
+//	}
+//
+//	MyVector(int memory_size, T data) : ISize(-1), memory_allocation(memory_size)
+//	{
+//		content = new T[memory_size];
+//		for (int i = 0; i < memory_size; ++i)
+//		{
+//			content[i] = data;
+//		}
+//	}
+//
+//	void push_back(T data)
+//	{
+//		if (ISize >= memory_allocation - 1)
+//		{
+//			T* temp = new T[memory_allocation * 2];
+//			for (int i = 0; i < ISize + 1; ++i)
+//			{
+//				temp[i] = content[i];
+//			}
+//			delete[] content;
+//			content = temp;
+//			memory_allocation *= 2;
+//
+//		}
+//		content[++ISize] = data;
+//
+//
+//		//배열의 마지막으로 데이터를 집어넣음
+//			//집어넣다가 길이가 부족해지면 *2
+//			//실제 들어있는 내용물과 할당된 메모리 크기는 다를 수 있음
+//	}
+//
+//	T pop_back()
+//	{
+//		if (ISize >= 0)
+//		{
+//			return content[ISize--];
+//		}
+//		else
+//		{
+//			std::cout << "비어있음" << std::endl;
+//			return NULL;
+//		}
+//	}
+//
+//	int size()
+//	{
+//		return ISize + 1;
+//	}
+//
+//	int capacity()
+//	{
+//		return memory_allocation;
+//	}
+//
+//	T operator[](int n)
+//	{
+//		return content[n];
+//	}
+//	
+//	~MyVector()
+//	{
+//		delete[] content;
+//	}
+//
+//};
+
+
 #include <iostream>
 
-template<typename T>
-class MyVector
+using namespace std;
+
+template <typename T, typename U> void Sort(U begin, U end, T cmp) //함수 오브젝트를 사용한 sort 함수 
 {
-	T* content;
-	int ISize;
-	int memory_allocation;
-
-public:
-	MyVector() : ISize(-1), memory_allocation(1)
+	for (auto i = begin; i != end; ++i)
 	{
-		content = new T[1];
-	}
-
-	MyVector(int memory_size) : ISize(-1), memory_allocation(memory_size)
-	{
-		content = new T[memory_size];
-	}
-
-	MyVector(int memory_size, T data) : ISize(-1), memory_allocation(memory_size)
-	{
-		content = new T[memory_size];
-		for (int i = 0; i < memory_size; ++i)
+		for (auto j = i; j != end; ++j)
 		{
-			content[i] = data;
+			if (cmp(*i, *j))
+			{
+				auto temp = *i;
+				*i = *j;
+				*j = temp;
+			}
 		}
 	}
+}
 
-	void push_back(T data)
+template <typename U> void Sort(U begin, U end) //함수 오브젝트를 사용하지 않은 sort 함수, algorithm 헤더 안에 있는 소트가 저렇게 두 개로 구현되어 있지 않을까,,
+{
+	for (auto i = begin; i != end; ++i)
 	{
-		if (ISize >= memory_allocation - 1)
+		for (auto j = i; j != end; ++j)
 		{
-			T* temp = new T[memory_allocation * 2];
-			for (int i = 0; i < ISize + 1; ++i)
+			if (*i < *j)
+			{
+				auto temp = *i;
+				*i = *j;
+				*j = temp;
+			}
+		}
+	}
+}
+
+
+template <typename T> class VECTOR_Iterator //adaptor 패턴 기법을 사용함, 자료구조는 그 자체로 있고 자료구조 밖에서 자료구조를 가리킴
+{
+	T* cursor; //대신해서 가리켜줄 포인터
+
+public:
+	VECTOR_Iterator(T* p = 0) : cursor(p) {}
+
+	bool operator!=(const VECTOR_Iterator& p)
+	{
+		return cursor != p.cursor;
+	}
+
+	bool operator==(const VECTOR_Iterator& p)
+	{
+		return cursor == p.cursor;
+	}
+
+	VECTOR_Iterator& operator++()
+	{
+		++cursor;
+		return *this;
+	}
+
+	VECTOR_Iterator& operator++(int)
+	{
+		cursor++;
+		return *this;
+	}
+
+	T& operator* ()
+	{
+		return *cursor;
+	}
+};
+
+template <typename T> class VECTOR //이건 그냥 뭐 벡터 구현
+{
+	T* content;
+	int mSize;
+	int mCapacity;
+	int mIndex;
+
+public:
+	VECTOR(int s = 0) : mSize(s), mCapacity(s), mIndex(0)
+	{
+		content = new T[mCapacity];
+	}
+
+	int size() { return mSize; }
+	int capacity() { return mCapacity; }
+
+	void resize(int s)
+	{
+		mSize = s;
+		if (s >= mCapacity)
+		{
+			mCapacity = s * 2;
+
+			T* temp = new T[mCapacity];
+			for (int i = 0; i < mSize; ++i)
 			{
 				temp[i] = content[i];
 			}
 			delete[] content;
 			content = temp;
-			memory_allocation *= 2;
-
-		}
-		content[++ISize] = data;
-
-
-		//배열의 마지막으로 데이터를 집어넣음
-			//집어넣다가 길이가 부족해지면 *2
-			//실제 들어있는 내용물과 할당된 메모리 크기는 다를 수 있음
-	}
-
-	T pop_back()
-	{
-		if (ISize >= 0)
-		{
-			return content[ISize--];
-		}
-		else
-		{
-			std::cout << "비어있음" << std::endl;
-			return NULL;
 		}
 	}
 
-	int size()
+	void push_back(const T& data) // 이렇게도 구현 하나보다.
 	{
-		return ISize + 1;
+		if (mIndex == mSize) resize(mSize + 1);
+		content[mIndex++] = data;
 	}
 
-	int capacity()
-	{
-		return memory_allocation;
-	}
-
-	T operator[](int n)
-	{
-		return content[n];
-	}
-	
-	~MyVector()
-	{
-		delete[] content;
-	}
-
+	typedef VECTOR_Iterator<T> iterator;
+	iterator begin() { return iterator(content); }
+	iterator end() { return iterator(content + mSize); }
 };
+
